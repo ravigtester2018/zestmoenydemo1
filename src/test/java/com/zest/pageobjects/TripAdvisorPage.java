9e0bb0fc2e9cf5487d9740c01c81a34e55e67ebf
ravitester <ravitester2018@gmail.com>
2019-12-10 16:16:13 +0530

    TripAdviser Initial Commit

---
+++ src/test/java/com/zest/pageobjects/TripAdvisorPage.java
@@ -0,0 +1,93 @@
+package com.zest.pageobjects;
+
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TripAdvisorPage extends BaseClass {
+
+	public TripAdvisorPage() {
+		super();
+	}
+
+	private void ExternalWaitCSS(String locator) {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOf(findElementByCSS(locator)));
+	}
+
+	String tsearchIconCSS = "span[class='brand-global-nav-action-search-Search__label--3PRUD']";
+	String tMainSearchBox = "#mainSearch";
+	String tFirstSearchResultCSS = "ul[class='resultContainer local']>li:nth-of-type(1) span[class='poi-name primaryText']";
+	 String tRatingBubbleCSS = "#bubble_rating";
+	String tWriteAReviewButtonCSS = "div[class='hotels-community-content-common-ContextualCTA__currentOption--3Wd5D'] a[href*='UserReview']";
+	 String tReviewTitle = "#ReviewTitle";
+	 String tReviewBox = "#ReviewText";
+	// String fsearchButtonXpath = "//button[@type='submit']";
+	// String fsearchBox = "input[title='Search for products, brands and
+	// more']";
+	// String fsearchBox1 = "input[name='q']";
+	// String fgetPriceCSS = "a[href*='apple-iphone-xr-yellow-64-gb']
+	// div[class='_1vC4OE _2rQ-NK']";
+	// String fclosePopupButtonCSS = "button[class='_2AkmmA _29YdH8']";
+
+	WebElement findElementByCSS(String locator) {
+		return driver.findElement(By.cssSelector(locator));
+	}
+
+	List<WebElement> findListOfElementsByCSS(String locator) {
+		return driver.findElements(By.cssSelector(locator));
+	}
+
+	WebElement findElementByXpath(String locator) {
+		return driver.findElement(By.xpath(locator));
+	}
+
+	public Boolean pageLoad(WebDriver driver, WebElement element) {
+		Boolean elementPresent = isElementPresent(driver, element);
+		if (elementPresent) {
+			System.out.println(driver.getCurrentUrl() + " - Page Loaded Successfully.");
+		} else {
+			System.out.println(driver.getCurrentUrl() + " - Page Not Loaded Properly.");
+		}
+		return elementPresent;
+	}
+
+	public void SearchAndGetFirstResult(WebDriver driver, String placeName) {
+		pageLoad(driver, findElementByCSS(tsearchIconCSS));
+		if (isElementPresent(driver, findElementByCSS(tsearchIconCSS))) {
+			findElementByCSS(tsearchIconCSS).click();
+			System.out.println("Search Icon Clicked...");
+			findElementByCSS(tMainSearchBox).sendKeys(placeName);
+			System.out.println("Place name entered in Main Search Box...");
+			findElementByCSS(tMainSearchBox).click();
+			System.out.println(" Search box clicked...");
+			ExternalWaitCSS(tFirstSearchResultCSS);
+			findElementByCSS(tFirstSearchResultCSS).click();
+			System.out.println("First Element clicked...");
+		}
+	}
+
+	public void WriteReview(WebDriver driver, String reviewTitle,String reviewText) {
+		String parent = driver.getWindowHandle();
+		findElementByCSS(tWriteAReviewButtonCSS).click();
+		System.out.println("Review button clicked...");
+		Set<String> windowHandles = driver.getWindowHandles();
+		for (String handle : windowHandles) {
+			if(!handle.equals(parent)){
+				driver.switchTo().window(handle);
+			}
+		}
+		System.out.println("Switch to new Tab");
+		Actions action = new Actions(driver);
+		action.moveToElement(findElementByCSS(tRatingBubbleCSS)).build().perform();
+		findElementByCSS(tReviewTitle).sendKeys(reviewTitle);
+		findElementByCSS(tReviewBox).sendKeys(reviewText);
+	}
+
+}
---
+++ src/test/java/com/zest/tests/TripAdvisorTest.java
@@ -0,0 +1,59 @@
+package com.zest.tests;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.zest.pageobjects.BaseClass;
+import com.zest.pageobjects.TripAdvisorPage;
+import com.zest.utilities.ReadPropertyFile;
+import com.zest.utilities.TextFormatting;
+
+public class TripAdvisorTest extends BaseClass {
+
+	Logger log = Logger.getLogger(TripAdvisorTest.class.getName());
+	ReadPropertyFile read;
+	TextFormatting format = new TextFormatting();
+
+	public TripAdvisorTest() {
+		super();
+	}
+
+	@BeforeTest
+	public void before() throws IOException {
+	}
+
+	@BeforeMethod
+	public void beforeMethod() throws IOException {
+		read = new ReadPropertyFile();
+		driver = BaseClass(read);
+
+	}
+
+	@Test
+	public void writeTripAdvisorReview() throws IOException {
+		TripAdvisorPage tAdvisorPage = new TripAdvisorPage();
+		getUrl(driver, read.returnPropertyValue("tripadvisor_url"));
+		// Search tripadvisor site
+		log.info("Trip Advisor Page Object Created");
+		tAdvisorPage.SearchAndGetFirstResult(driver,"Club Mahindra");
+		tAdvisorPage.WriteReview(driver, "Test Review", "Some review added");
+//		aprice = aloginPage.AmazonSearchItemAndGetPrice(driver, "iphone xr 64gb - yellow", 0);
+//		log.info("Item searched in Amazon and got the price." + aprice);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		driver.quit();
+	}
+
+}
